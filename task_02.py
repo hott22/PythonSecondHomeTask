3c2107e8cc2f65a593e462b5b19b2fc427a96b86
Bobby <mif210210@gmail.com>
2022-05-17 17:28:55 +0300

    проверка на ввод числа вместо буквы и в диапазоне

--- task_02.py
+++ task_02.py
@@ -7,7 +7,7 @@
 # b) Подумайте как наделить бота "интеллектом"
 import random
 import time
-import numbers
+
 
 limit_candies = 5  # лимит конфет
 number_of_candies = 25  # количество конфет
@@ -21,22 +21,24 @@ appeals_end_lose = ['Увы :-(', 'Не огорчайся! Но ты проиг
                     'Проигрыш, попробуй еще','На этот раз бот оказался сильнее']
 appeals_end_won = ['Ура, ты победил :-)','Сегодня твой день, поздравляю!','Ты выиграл!','Победа!!!',
                     'Удача на твоец стороне!','Хорошо умеешь считать!','Забирай все конфеты!']
-
-def user_move(count, limit, user):
+appeals_not_correctnumber = [f'Давай все таки число не больше {limit_candies}: ', 'Эй, хочешь поломать программу? Давай число: ',
+                    'Жду правильное число: ']
+def user_move(count, limit_candies, user):
     if user == 1:
-        a = int(input(f'{appeals_user[random.randint(0, len(appeals_user) - 1)]}'))
+        a = input(f'{appeals_user[random.randint(0, len(appeals_user) - 1)]}')
+        while a.isdigit() == False or int(a) > limit_candies or int(a) < 1:
+            a = input(f'{appeals_not_correctnumber[random.randint(0, len(appeals_not_correctnumber) - 1)]}')
     elif user == 2:
-        a = int(input(f'{first_user} {appeals_user[random.randint(0, len(appeals_user) - 1)]}'))
+        a = input(f'{appeals_user[random.randint(0, len(appeals_user) - 1)]}')
+        while a.isdigit() == False or int(a) > limit_candies or int(a) < 1:
+            a = input(f'{first_user} {appeals_not_correctnumber[random.randint(0, len(appeals_not_correctnumber) - 1)]}')
     elif user == 3:
-        a = int(input(f'{second_user} {appeals_user[random.randint(0, len(appeals_user) - 1)]}'))
- 
-
-    while a > limit:
-        a = int(input(f'Перебор, не более {limit}, осталось {count} попробуй еще: '))
-    count -= a
-    
-
+        a = input(f'{appeals_user[random.randint(0, len(appeals_user) - 1)]}')
+        while a.isdigit() == False or int(a) > limit_candies or int(a) < 1:
+            a = input(f'{second_user} {first_user} {appeals_not_correctnumber[random.randint(0, len(appeals_not_correctnumber) - 1)]}')
+    count -= int(a)
     return count
+
 
 def bot_move(count, limit, choice):
     correct_count = count % (limit + 1)
@@ -92,35 +94,26 @@ while True:
           '\n3 - Выйти из игры'
           '\n')
 
-    temp_choice = input('Введи номер операции: ')
-    while temp_choice.isnumeric() == False:
-        temp_choice = input('Нужно ввести число от 1 до 3: ')
-    else:
-        my_choice = int(temp_choice)
-        while my_choice > 3:
-            my_choice = int(input('Перебор не более 3, введи заново: '))
-    if my_choice == 3:
+    my_choice = input('Введи номер операции: ')
+    while my_choice.isdigit() == False or int(my_choice) > 3 or int(my_choice) < 1:
+        my_choice = input('Нужно ввести только число от 1 до 3: ')
+    if int(my_choice) == 3:
         break
-    elif my_choice == 1:
+    elif int(my_choice) == 1:
         print(f'На столе лежит {number_of_candies} конфет(а). Играют два игрока делая ход друг после друга.'
               '\nПервый ход определяется жеребьёвкой.'
               f'За один ход можно забрать не более чем {limit_candies} конфет.'
               '\nВсе конфеты оппонента достаются сделавшему последний ход.'
               '\n')
-    elif my_choice == 2:
+    elif int(my_choice) == 2:
         print('1 - Играть с ботом'
             "\n2 - Играть с оппонентом"
             '\n')
         
-        temp_choice = input('С кем хочешь играть? ')
-        while temp_choice.isnumeric() == False:
-            temp_choice = input('Нужно ввести число от 1 до 2: ')
-        else:
-            my_choice2 = int(temp_choice)
-            while my_choice2 > 2:
-                my_choice2 = int(input('Перебор не более 2, введи заново: '))
-
-        if my_choice2 == 2:
+        my_choice2 = input('С кем хочешь играть? ')
+        while my_choice2.isdigit() == False or int(my_choice2) > 2 or int(my_choice2) < 1:
+            my_choice2 = input('Нужно ввести число от 1 до 2: ')
+        if int(my_choice2) == 2:
             first_user = input('Введи имя первого игрока: ')
             second_user = input('Введи имя второго игрока: ')
             draw = random.randint(0, 1)
@@ -169,13 +162,9 @@ while True:
             print('1 - легкий'
                 '\n2 - средний'
                 '\n3 - тяжелый')
-            temp_choice = input('Какой уровень сложности выбираешь? ')
-            while temp_choice.isnumeric() == False:
-                temp_choice = input('Нужно ввести число от 1 до 2: ')
-            else:
-                my_choice3 = int(temp_choice)
-                while my_choice3 > 3:
-                    my_choice3 = int(input('Перебор не более 3, введи заново: '))
+            my_choice3 = input('Какой уровень сложности выбираешь? ')
+            while my_choice3.isdigit() == False or int(my_choice3) > 3 or int(my_choice3) < 1:
+                my_choice3 = input('Нужно ввести число от 1 до 3: ')
             draw = random.randint(0, 1)
             slow_printing()
             print()
@@ -188,14 +177,14 @@ while True:
                 if draw == 0:
                     number_of_candies = user_move(number_of_candies, limit_candies, 1)
                 else:
-                    number_of_candies = bot_move(number_of_candies, limit_candies, my_choice3)
+                    number_of_candies = bot_move(number_of_candies, limit_candies, int(my_choice3))
                 if number_of_candies <= limit_candies:
                     break
                 number_moves += 1
                 print(f'Осталось конфет: {number_of_candies}')
                 print()
                 if draw == 0:
-                    number_of_candies = bot_move(number_of_candies, limit_candies, my_choice3)
+                    number_of_candies = bot_move(number_of_candies, limit_candies, int(my_choice3))
                 else:
                     number_of_candies = user_move(number_of_candies, limit_candies, 1)
                 if number_of_candies <= limit_candies:
